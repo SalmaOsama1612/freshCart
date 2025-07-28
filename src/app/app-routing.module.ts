4cf42f64b36eee8fbbd9e8417b0b409781dace5e
SalmaOsama1612 <salmaosamaas287@gmail.com>
2025-07-29 02:38:48 +0300

    the routing pathes and not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,95 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/blank-layout/blank-layout.component').then(
+        (m) => m.BlankLayoutComponent
+      ),
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./components/home/home.component').then(
+            (m) => m.HomeComponent
+          ),
+        title: 'home',
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./components/products/products.component').then(
+            (m) => m.ProductsComponent
+          ),
+        title: 'products',
+      },
+      {
+        path: 'cart',
+        loadComponent: () =>
+          import('./components/cart/cart.component').then(
+            (m) => m.CartComponent
+          ),
+        title: 'cart',
+      },
+      {
+        path: 'brands',
+        loadComponent: () =>
+          import('./components/brands/brands.component').then(
+            (m) => m.BrandsComponent
+          ),
+        title: 'brands',
+      },
+      {
+        path: 'categories',
+        loadComponent: () =>
+          import('./components/categories/categories.component').then(
+            (m) => m.CategoriesComponent
+          ),
+        title: 'categories',
+      },
+    ],
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./components/login/login.component').then(
+            (m) => m.LoginComponent
+          ),
+        title: 'login',
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./components/register/register.component').then(
+            (m) => m.RegisterComponent
+          ),
+        title: 'register',
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/notfound/notfound.component').then(
+        (m) => m.NotfoundComponent
+      ),
+    title: 'Not Found',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
